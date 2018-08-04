b8c104801e8a81fb655f7af1553e12dcd5c2436c
Philippe Miranda <miranda.philippe@gmail.com>
2018-08-03 21:27:52 -0300

    Commit inicial para aula do gil

---
+++ js/main.js
@@ -0,0 +1,23 @@
+config = {
+  apiKey: "AIzaSyC8pjQpOh2K_XcBhvYpk6gifzCaZ585IlI",
+  authDomain: "philippe-portifolio.firebaseapp.com",
+  databaseURL: "https://philippe-portifolio.firebaseio.com",
+  projectId: "philippe-portifolio",
+  storageBucket: "philippe-portifolio.appspot.com",
+  messagingSenderId: "853016257158"
+};
+
+firebase.initializeApp(config);
+
+
+  let _db =  firebase.firestore();
+  let jobList = [];
+
+  function getAllJobs() {
+    _db.collection('jobs').orderBy('date', 'desc').get().then(
+      (listItem) => {jobList.push(listItem)}
+    ).catch(
+      err => alert(err)
+    )
+  }
+
